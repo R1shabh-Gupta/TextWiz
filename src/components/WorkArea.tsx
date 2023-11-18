fafb0e01cc9b319ad06dfe2ba41f165772629ae0
rishabh <rishabhgupta.ks3@gmail.com>
2023-11-19 00:25:56 +0530

    Added OCR feature

--- src/components/WorkArea.tsx
+++ src/components/WorkArea.tsx
@@ -13,6 +13,18 @@ import { Label } from './ui/label';
 import { Textarea } from './ui/textarea';
 import { Signup } from './Signup';
 import { Login } from './Login';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog';
+import { Input } from './ui/input';
+// import console from 'console';
+import Tesseract from 'tesseract.js';
 
 type appProps = {
   onIsLoggedIn: (value: boolean) => void;
@@ -26,6 +38,7 @@ const WorkArea = ({ onIsLoggedIn }: appProps) => {
   const [isLoading, setIsLoading] = useState(false);
   const [isCopy, setIsCopy] = useState('Copy');
   const [isCopyIcon, setIsCopyIcon] = useState(false);
+  const [picture, setPicture] = useState<File>();
 
   //Funtionality functions
   const toUppercase = (text: string) => {
@@ -185,6 +198,61 @@ const WorkArea = ({ onIsLoggedIn }: appProps) => {
     }
   };
 
+  const onAddingImg = (event: React.FormEvent) => {
+    const files = (event.target as HTMLInputElement).files;
+
+    if (files && files.length > 0) {
+      setPicture(files[0]);
+      console.log(picture);
+    }
+  };
+
+  function pressEscapeKey() {
+    const escKeyEvent = new KeyboardEvent('keydown', {
+      key: 'Escape',
+      bubbles: true,
+      cancelable: true,
+    });
+
+    document.dispatchEvent(escKeyEvent);
+  }
+
+  const handleOCR = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    try {
+      event.preventDefault();
+      setIsLoading(true);
+      if (picture) {
+        const result = await performOCR(picture);
+        // Update the state with the OCR result
+        setInputText(result);
+        pressEscapeKey();
+        setIsLoading(false);
+        // console.log(result);
+      } else {
+        console.error('No image selected');
+      }
+    } catch (error) {
+      console.error('Error summarizing text:', error);
+    }
+  };
+
+  const performOCR = async (imageFile: File) => {
+    try {
+      const {
+        data: { text },
+      } = await Tesseract.recognize(imageFile, 'eng', {
+        logger: (info) => console.log(info),
+      });
+
+      return text;
+    } catch (error) {
+      console.error('Error:', error);
+      throw error;
+    }
+  };
+
   const handleKeyword = async () => {
     if (user) {
       try {
@@ -253,10 +321,46 @@ const WorkArea = ({ onIsLoggedIn }: appProps) => {
         {/* TextArea */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mt-8 w-[80%]">
           {/* Input Textarea */}
-          <div className="flex flex-col gap-2">
-            <Label htmlFor="message" className="text-lg mb-2">
-              Input Text
-            </Label>
+          <div className="flex flex-col">
+            <div className="flex justify-between mb-2 items-center">
+              <Label htmlFor="message" className="text-lg">
+                Input Text
+              </Label>
+              {/* OCR */}
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button variant="outline">OCR</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Image to Text Magic</DialogTitle>
+                    <DialogDescription>
+                      Revolutionize your workflow with our OCR feature,
+                      effortlessly transform images into editable text.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="name" className="text-right">
+                        Upload file
+                      </Label>
+                      <Input
+                        id="name"
+                        className="col-span-3"
+                        type="file"
+                        accept="image/*"
+                        onChange={onAddingImg}
+                      />
+                    </div>
+                  </div>
+                  <DialogFooter>
+                    <Button type="submit" onClick={handleOCR}>
+                      {isLoading ? 'Processing' : 'Submit'}
+                    </Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            </div>
             <Textarea
               className="placeholder:italic placeholder:text-slate-400 resize-y"
               placeholder="Type your message here..."
@@ -270,7 +374,7 @@ const WorkArea = ({ onIsLoggedIn }: appProps) => {
 
           {/* Output Textarea */}
           <div className="flex flex-col">
-            <div className="flex justify-between">
+            <div className="flex justify-between items-center">
               <Label htmlFor="message" className="text-lg">
                 Output Text
               </Label>
